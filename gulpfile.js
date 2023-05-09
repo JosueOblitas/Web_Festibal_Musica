42dc46a116e217b584744670338fa8c020a3a4fe
Josue Oblitas <josueoblitas15@gmail.com>
2023-05-09 08:33:09 -0500

    segundo commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-function tarea(done){
-    console.log('Hola mundo');
+const {src,dest,watch} = require('gulp');
+const sass = require("gulp-sass")(require("sass"));
+function css(done){
+    //1.-Identificar el archivo de sass
+    //2.-Compilar el archivo sass
+    //3.-Alamacenar en el disco duro
+    
+    //1                         //2          //3
+    src('src/scss/**/*.scss').pipe( sass() ).pipe( dest('build/css') );
     done();
 }
-
-exports.tarea = tarea;
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+    done();
+}
+exports.css = css;
+exports.dev = dev;
