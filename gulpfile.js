b7f7b1e14d9fca378c2e4fc6a640bacc04648f8a
unknown <josueoblitas15@gmail.com>
2023-05-16 19:04:04 -0500

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 const {src,dest,watch} = require('gulp');
+//CSS
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require('gulp-plumber');
+
+//iMAGENES
+const webp = require('gulp-webp');
+
 function css(done){
     //1.-Identificar el archivo de sass
     //2.-Compilar el archivo sass
     //3.-Alamacenar en el disco duro
     
-    //1                         //2          //3
-    src('src/scss/**/*.scss').pipe( sass() ).pipe( dest('build/css') );
+    //1                                  
+    src('src/scss/**/*.scss')
+    .pipe( plumber() )
+    .pipe( sass() ) //2
+    .pipe( dest('build/css') ); //3
+    done();
+}
+function versionWebp(done){
+    src()
     done();
 }
 function dev(done){
