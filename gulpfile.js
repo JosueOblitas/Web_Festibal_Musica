33eccb2c78e5950a97f91c585c9a3741aeac49bf
unknown <josueoblitas15@gmail.com>
2023-05-29 11:00:49 -0500

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
-const {src,dest,watch} = require('gulp');
+const {src,dest,watch,parallel} = require('gulp');
 //CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps'); 
+
+// JavaScript
+const terser = require('gulp-terser-js');
 
 //iMAGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     //1.-Identificar el archivo de sass
@@ -13,18 +23,58 @@ function css(done){
     
     //1                                  
     src('src/scss/**/*.scss')
+    .pipe( sourcemaps.init() )
     .pipe( plumber() )
     .pipe( sass() ) //2
+    .pipe(postcss([autoprefixer(),cssnano()]))
+    .pipe( sourcemaps.write('.') )
     .pipe( dest('build/css') ); //3
     done();
 }
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin(opciones) ) )
+    .pipe( dest('build/img') );
+}
+
 function versionWebp(done){
-    src()
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img') );
+    done();
+}
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe( avif(opciones) )
+    .pipe( dest('build/img') );
+    done();
+}
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe( sourcemaps.init() )
+    .pipe( terser() )
+    .pipe( sourcemaps.write('.') )
+    .pipe( dest('build/js') );
     done();
 }
 function dev(done){
     watch('src/scss/**/*.scss',css);
+    watch('src/js/**/*.js',javascript);
     done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes,versionWebp,versionAvif,javascript,dev);
